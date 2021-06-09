87675664ef6898c572645c8a5d19ae7e552460ed
Matt Murphy <matthew.murphy@cognite.com>
2021-06-09 16:11:41 +0200

    Remove unnecessary deps demo-2

--- demo-2/index.js
+++ demo-2/index.js
@@ -1,14 +1,11 @@
-const typeDefs = require('./graphql-schema').typeDefs
-const ApolloServer = require('apollo-server-express').ApolloServer
-const express = require('express')
-const neo4j = require('neo4j-driver')
-const Neo4jGraphQL = require('@neo4j/graphql').Neo4jGraphQL
-const dotenv = require('dotenv')
+const { typeDefs } = require('./graphql-schema');
+const { ApolloServer } = require('apollo-server');
+const neo4j = require('neo4j-driver');
+const { Neo4jGraphQL } = require('@neo4j/graphql');
+const dotenv = require('dotenv');
 
 // set environment variables from .env
-dotenv.config()
-
-const app = express()
+dotenv.config();
 
 /*
  * Create a Neo4j driver instance to connect to the database
@@ -21,7 +18,7 @@ const driver = neo4j.driver(
     process.env.NEO4J_USER || 'neo4j',
     process.env.NEO4J_PASSWORD || 'neo4j'
   )
-)
+);
 
 /*
  * Create an executable GraphQL schema object from GraphQL type definitions
@@ -32,7 +29,7 @@ const driver = neo4j.driver(
 const neoSchema = new Neo4jGraphQL({
   typeDefs,
   driver,
-})
+});
 
 /*
  * Create a new ApolloServer instance, serving the GraphQL schema
@@ -48,19 +45,8 @@ const server = new ApolloServer({
   schema: neoSchema.schema,
   introspection: true,
   playground: true,
-})
+});
 
-// Specify host, port and path for GraphQL endpoint
-const port = 4001
-const path = '/graphql'
-const host = '0.0.0.0'
-
-/*
- * Optionally, apply Express middleware for authentication, etc
- * This also also allows us to specify a path for the GraphQL endpoint
- */
-server.applyMiddleware({ app, path })
-
-app.listen({ host, port, path }, () => {
-  console.log(`GraphQL server ready at http://${host}:${port}${path}`)
-})
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
