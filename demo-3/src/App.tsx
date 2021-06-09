38e844a81c059fc67f4ca128f2bc657ae3199b79
Matt Murphy <matthew.murphy@cognite.com>
2021-06-09 16:12:26 +0200

    Add in live code to demo-3 react app

--- demo-3/src/App.tsx
+++ demo-3/src/App.tsx
@@ -1,15 +1,31 @@
 import "./App.css";
 import BarbersList from "./components/BarbersList";
 import AddClientToBarberForm from "./components/AddClientToBarberForm";
+import { useAddClientToBarberMutation, useCreateClientMutation, useGetBarbersQuery } from "./codegen/hooks";
 
 function App() {
+  const { data, loading, error, refetch } = useGetBarbersQuery();
+
+  const [createClientMutation] = useCreateClientMutation();
+  const [addClientToBarberMutation] = useAddClientToBarberMutation();
 
   const handleAddClientClick = async (
     barberName: string,
     clientName: string
   ) => {
+    await createClientMutation({ variables: { name: clientName } });
+    await addClientToBarberMutation({ variables: { clientName, barberName } });
+    refetch();
 
   };
+
+  if (loading) {
+    return <h3>Loading...</h3>
+  }
+
+  if (error) {
+    return <h3>Error, try refresh</h3>
+  }
 
   return (
     <div>
@@ -17,7 +33,7 @@ function App() {
         <h1>Matt's Codegen Barbershop</h1>
       </header>
       <div className="content">
-        <BarbersList barbers={[]} />
+        <BarbersList barbers={data?.barbers || []} />
         <AddClientToBarberForm onSubmit={handleAddClientClick} />
       </div>
     </div>
